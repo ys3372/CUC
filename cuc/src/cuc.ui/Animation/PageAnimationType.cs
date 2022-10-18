735f1d2014dc9958e1f71a9c89671240a264e1ad
ys3372 <ys3372@columbia.edu>
2022-10-18 18:43:04 +0800

    1018

---
+++ cuc/src/cuc.ui/Animation/PageAnimationType.cs
@@ -0,0 +1,12 @@
+﻿namespace cuc.ui
+{
+    /// <summary>
+    /// The page animation type
+    /// </summary>
+    public enum PageAnimationType
+    {
+        None = 0,
+        Fade = 1,
+        Slide =2,
+    }
+}
